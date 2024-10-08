8526e1660a512326ef48bc814b6682fd44385e02
Aarti Yadav <aartiyadav2405@gmail.com>
2024-10-08 08:06:45 +0530

    correct import stmt

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Header from './component/Header.jsx'
 import Home from './component/Home.jsx'
 import Footer from './component/footer/Footer.jsx'
-import Schedule from './component/schedule/schedule.jsx'
+import Schedule from './component/schedule/Schedule.jsx'
 import Pricing from './component/pricing/Pricing.jsx'
 import './App.css'
 
